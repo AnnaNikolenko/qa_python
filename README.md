ccc4041cb9e531c4f0089f4607081a73ee8d5279
AnnaNikolenko <49036479+AnnaNikolenko@users.noreply.github.com>
2023-01-29 00:46:01 +0300

    Tests + Readme file are added

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# qa_python
+# conftest.py
+В файле создана фикстура, создающая экземпляр класса BooksCollector перед каждым тестом
+
+# test_add_new_book_add_two_books
+Проверяет, что в коллекцию books_rating добавились две книги
+
+# test_default_value_books_rating_empty
+Проверяет, что параметр books_rating по умолчанию пустой
+
+# test_default_value_favorites_empty
+Проверяет, что параметр favorites по умолчанию пустой
+
+# test_add_new_book_is_added
+Проверяет, что в словаре добавлена книга с рейтингом 1
+
+# test_set_book_rating_successful
+Проверяет, что установлен рейтинг книге (от 1 до 10)
+
+# test_get_book_rating_successful
+Проверяет, что можно получить рейтинг книги по ее имени
+
+# test_get_books_with_specific_rating_successful
+Проверяет, что доступен вывод списка книг с определенным рейтингом
+
+# test_get_books_rating_successful
+Проверяет, что можно получить словарь books_rating
+
+# test_add_book_in_favorites_successful
+Проверяет, что книгу можно добавить в избранное
+
+# test_delete_book_from_favorites_successful
+Проверяет, что книгу можно удалить из избранного
+
+# test_get_list_of_favorites_books_successful
+Проверяет, что можно получить список избранных книг
+
+
+
+
--- tests.py
+++ tests.py
@@ -1,24 +1,74 @@
-from main import BooksCollector
+name = "Вино из одуванчиков"
+name2 = "Время колокольчиков"
+rating = 7
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
+    # проверяем, что добавились две книги
+    def test_add_new_book_add_two_books(self, collector):
+        collector.add_new_book(name)
+        collector.add_new_book(name2)
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    # проверяем, что параметр books_rating по умолчанию пустой
+    def test_default_value_books_rating_empty(self, collector):
+        assert collector.books_rating == {}
+
+    # проверяем, что параметр favorites по умолчанию пустой
+    def test_default_value_favorites_empty(self, collector):
+        assert collector.favorites == []
+
+    # проверяем, что в словаре добавлена книга с рейтингом 1
+    def test_add_new_book_is_added(self, collector):
+        collector.add_new_book(name)
+        assert collector.books_rating[name] == 1
+
+    # проверяем, что установлен рейтинг книге (от 1 до 10)
+    def test_set_book_rating_successful(self, collector):
+        collector.add_new_book(name)
+        collector.set_book_rating(name, rating)
+        assert collector.books_rating[name] == rating
+
+    # проверяем, что можно получить рейтинг книги по ее имени
+    def test_get_book_rating_successful(self, collector):
+        collector.add_new_book(name)
+        collector.set_book_rating(name, rating)
+        assert collector.get_book_rating(name) == rating
+
+    # проверяем, что доступен вывод списка книг с определенным рейтингом
+    def test_get_books_with_specific_rating_successful(self, collector):
+        collector.add_new_book(name)
+        collector.set_book_rating(name, rating)
+        assert collector.get_books_with_specific_rating(rating) == ['Вино из одуванчиков']
+
+    # проверяем, что можно получить словарь books_rating
+    def test_get_books_rating_successful(self, collector):
+        collector.add_new_book(name)
+        collector.set_book_rating(name, rating)
+        print(collector.get_books_rating())
+        assert collector.get_books_rating() == {'Вино из одуванчиков': 7}
+
+    # проверяем, что книгу можно добавить в избранное
+    def test_add_book_in_favorites_successful(self, collector):
+        collector.add_new_book(name)
+        collector.add_book_in_favorites(name)
+        assert collector.favorites[0] == 'Вино из одуванчиков'
+
+    # проверяем, что книгу можно удалить из избранного
+    def test_delete_book_from_favorites_successful(self, collector):
+        collector.add_new_book(name)
+        collector.add_book_in_favorites(name)
+        collector.delete_book_from_favorites(name)
+        assert collector.favorites == []
+
+    # проверяем, что можно получить список избранных книг
+    def test_get_list_of_favorites_books_successful(self, collector):
+        collector.add_new_book(name)
+        collector.add_new_book(name2)
+        collector.add_book_in_favorites(name)
+        collector.add_book_in_favorites(name2)
+        print(collector.favorites)
+        assert collector.get_list_of_favorites_books() == ['Вино из одуванчиков', 'Время колокольчиков']
+
+
